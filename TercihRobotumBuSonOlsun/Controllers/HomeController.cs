0dd08e28c195817d0b6711e7038a431cd8c933cb
mustafa kemal sarı <kemal_sari_267@hotmail.com>
2018-12-25 03:37:15 +0300

    v.9

--- TercihRobotumBuSonOlsun/Controllers/HomeController.cs
+++ TercihRobotumBuSonOlsun/Controllers/HomeController.cs
@@ -17,82 +17,105 @@ namespace IdentitySample.Controllers
         TercihContext tercihContext = new TercihContext();
         List<TercihVeriModel> localList = new List<TercihVeriModel>();
 
-        [HttpGet]
-        public ActionResult Index(string sortOrder, string aramaMetni, string currentFilter, int? page, bool universiteDevlet = false)
+
+        public ActionResult Index( FilitreModels models)
         {
 
+            int pageNumber = (models.Page ?? 1);
+            models.TercihVerileri = tercihContext.TercihVerileri.Where(f =>
+            (String.IsNullOrEmpty(models.AramaMetni) || f.ProgramAdi.Contains(models.AramaMetni)) &&
+            (!models.Burslu || f.ProgramAdi.Contains("Burslu")) &&
+            (!models.KKTC || f.ProgramAdi.Contains("KKTC-")) &&
+            (!models.Tm || f.PuanTuru.Contains("EA"))&&
+            (!models.Tyt || f.PuanTuru.Contains("TYT")) &&
+            (!models.Mf || f.PuanTuru.Contains("SAY")) &&
+            (!models.Ts || f.PuanTuru.Contains("SÖZ")) &&
+            (!models.Dil || f.PuanTuru.Contains("DİL"))
+        
 
 
-            bool test = universiteDevlet;
 
-            ViewBag.SortingKodaGore = String.IsNullOrEmpty(sortOrder) ? "Koda_Gore" : "";
-            ViewBag.SortingAdaGore = String.IsNullOrEmpty(sortOrder) ? "Ada_Gore" : "";
-            ViewBag.SortingPuanTuruneGore = String.IsNullOrEmpty(sortOrder) ? "Puan_Turune_Gore" : "";
-            ViewBag.SortingKontenjanaGore = String.IsNullOrEmpty(sortOrder) ? "Kontenjana_Gore" : "";
-            ViewBag.SortingYerleseneGore = String.IsNullOrEmpty(sortOrder) ? "Yerlesene_Gore" : "";
-            ViewBag.SortingEnKucukPuanaGore = String.IsNullOrEmpty(sortOrder) ? "En_Kucuk_Puana_Gore" : "";
-            ViewBag.SortingEnBuyukPuanaGore = String.IsNullOrEmpty(sortOrder) ? "En_Buyuk_Puana_Gore" : "";
-            
-            ViewBag.CurrentSort = sortOrder;
-            if (aramaMetni != null)
-            { page = 1; }
-            else
-            { aramaMetni = currentFilter; }
-            ViewBag.CurrentFilter = aramaMetni;
 
-            localList = tercihContext.TercihVerileri.ToList();
 
-          
-            if (!String.IsNullOrEmpty(aramaMetni))
+            ).OrderByDescending(f => f.EnBuyukPuan).ToPagedList(pageNumber, 20);
+            //select new TercihVeriModel
+            //{
+            //    ProgramKodu=cust.ProgramKodu,
+            //    ProgramAdi = cust.ProgramAdi,
+            //    PuanTuru=cust.PuanTuru,
+            //    Kontenjan=cust.Kontenjan,
+            //    Yerlesen=cust.Yerlesen,
+            //    EnBuyukPuan=cust.EnBuyukPuan,
+            //    EnKucukPuan=cust.EnKucukPuan
+                
+
+
+            //}).ToPagedList(pageNumber,20);
+
+            if (Request.IsAjaxRequest())
             {
-                localList = localList.Where(s => s.ProgramAdi.ToLower().Contains(aramaMetni.ToLower()) ||
-                s.ProgramKodu.ToLower().Contains(aramaMetni.ToLower())).ToList();
-
+                return PartialView("_TercihVerileri", models);
             }
-            // Tablodaki Sıralamaları Yapıyoruz
-            switch (sortOrder)
-            {
-                case "Koda_Gore":
-                    localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.ProgramKodu).ToList();
-                    break;
-                case "Ada_Gore":
-                    localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.ProgramAdi).ToList();
-                    break;
-                case "Puan_Turune_Gore":
-                    localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.PuanTuru).ToList();
-                    break;
-                case "Kontenjana_Gore":
-                    localList = localList.OrderByDescending(sayfalar_logtut => int.Parse(sayfalar_logtut.Kontenjan)).ToList();
-                    break;
-                case "Yerlesene_Gore":
-                    localList = localList.OrderByDescending(sayfalar_logtut => int.Parse(sayfalar_logtut.Yerlesen)).ToList();
-                    break;
-                case "En_Kucuk_Puana_Gore":
-                    localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.EnKucukPuan).ToList();
-                    break;
-                case "En_Buyuk_Puana_Gore":
-                    localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.EnBuyukPuan).ToList();
-                    break;
-                default:
-                    localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.ProgramKodu).ToList();
-                    break;
-            }
-            int pageSize = 20;
-            int pageNumber = (page ?? 1);
 
-            return View(localList.ToPagedList(pageNumber, pageSize));
+            return View(models);
+
+            //ViewBag.SortingKodaGore = String.IsNullOrEmpty(sortOrder) ? "Koda_Gore" : "";
+            //ViewBag.SortingAdaGore = String.IsNullOrEmpty(sortOrder) ? "Ada_Gore" : "";
+            //ViewBag.SortingPuanTuruneGore = String.IsNullOrEmpty(sortOrder) ? "Puan_Turune_Gore" : "";
+            //ViewBag.SortingKontenjanaGore = String.IsNullOrEmpty(sortOrder) ? "Kontenjana_Gore" : "";
+            //ViewBag.SortingYerleseneGore = String.IsNullOrEmpty(sortOrder) ? "Yerlesene_Gore" : "";
+            //ViewBag.SortingEnKucukPuanaGore = String.IsNullOrEmpty(sortOrder) ? "En_Kucuk_Puana_Gore" : "";
+            //ViewBag.SortingEnBuyukPuanaGore = String.IsNullOrEmpty(sortOrder) ? "En_Buyuk_Puana_Gore" : "";
+
+            //ViewBag.CurrentSort = sortOrder;
+            //if (aramaMetni != null)
+            //{ page = 1; }
+            //else
+            //{ aramaMetni = currentFilter; }
+            //ViewBag.CurrentFilter = aramaMetni;
+
+            //localList = tercihContext.TercihVerileri.ToList();
+
+
+            ////if (!String.IsNullOrEmpty(aramaMetni))
+            ////{
+            ////    localList = localList.Where(s => s.ProgramAdi.ToLower().Contains(aramaMetni.ToLower()) ||
+            ////    s.ProgramKodu.ToLower().Contains(aramaMetni.ToLower())).ToList();
+
+            ////}
+            //// Tablodaki Sıralamaları Yapıyoruz
+            //switch (sortOrder)
+            //{
+            //    case "Koda_Gore":
+            //        localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.ProgramKodu).ToList();
+            //        break;
+            //    case "Ada_Gore":
+            //        localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.ProgramAdi).ToList();
+            //        break;
+            //    case "Puan_Turune_Gore":
+            //        localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.PuanTuru).ToList();
+            //        break;
+            //    case "Kontenjana_Gore":
+            //        localList = localList.OrderByDescending(sayfalar_logtut => int.Parse(sayfalar_logtut.Kontenjan)).ToList();
+            //        break;
+            //    case "Yerlesene_Gore":
+            //        localList = localList.OrderByDescending(sayfalar_logtut => int.Parse(sayfalar_logtut.Yerlesen)).ToList();
+            //        break;
+            //    case "En_Kucuk_Puana_Gore":
+            //        localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.EnKucukPuan).ToList();
+            //        break;
+            //    case "En_Buyuk_Puana_Gore":
+            //        localList = localList.OrderByDescending(sayfalar_logtut => sayfalar_logtut.EnBuyukPuan).ToList();
+            //        break;
+            //    default:
+            //        localList = localList.OrderBy(sayfalar_logtut => sayfalar_logtut.ProgramKodu).ToList();
+            //        break;
+            //}
+
+
+
         }
-        [HttpPost]
-        [Authorize]
-        public ActionResult Index(TercihVeriModel model) {
-
-            ViewBag.data = model;
-            
-           string test = model.ProgramKodu;
-            return View();
-
-            
-        }
+     
         [HttpGet]
         [Authorize]
         public ActionResult About()
--- TercihRobotumBuSonOlsun/Models/FilitreModels.cs
+++ TercihRobotumBuSonOlsun/Models/FilitreModels.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PagedList;
 
 namespace TercihRobotumBuSonOlsun.Models
 {
     public class FilitreModels
     {
-
-
+        
+        public bool Burslu { get; set; }
+        public bool KKTC { get; set; }
         public bool Devlet { get; set; }
         public bool Vakif { get; set; }
-        public bool Burslu { get; set; }
-        public bool YurtDisi { get; set; }
-        public bool KKTC { get; set; }
+
+       
+
         public bool Lisans { get; set; }
         public bool OnLisans { get; set; }
         public bool Tm { get; set; }
@@ -21,8 +23,23 @@ namespace TercihRobotumBuSonOlsun.Models
         public bool Mf { get; set; }
         public bool Ts { get; set; }
         public bool Dil { get; set; }
-        public double PuanMin { get; set; }
-        public double PuanMax { get; set; }
+        public string PuanMin { get; set; }
+        public string PuanMax { get; set; }
+        public string Ingilizce { get; set; }
+        public string Turkce { get; set; }
+
+        //public string ProgramKodu { get; set; }
+        //public string ProgramAdi { get; set; }
+        //public string PuanTuru { get; set; }
+        //public string Kontenjan { get; set; }
+        //public string Yerlesen { get; set; }
+        //public string EnKucukPuan { get; set; }
+        //public string EnBuyukPuan { get; set; }
+
+        public string AramaMetni { get; set; }
+        public int? Page { get; set; }
+
+        public IPagedList<TercihVeriModel> TercihVerileri { get; set; }
 
     }
 }
